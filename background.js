3971d7156fd5abb5f98e2a9d6f6bfd4a2d4c7955
Johannes Kunz <johannes.f.kunz@gmail.com>
2017-01-29 00:37:45 +0100

    Stable Redirect

--- background.js
+++ background.js
@@ -1,22 +1,42 @@
 var blacklist = ["conrad","amazon","youtube"];
 var whitelist = ["youtube.com"];
+
+
 setInterval(function(){
-    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-        var url = tabs[0].url;
-        var forbidden = false;
-        for(var i=0;i<blacklist.length;i++){
-            if(url.search(blacklist[i]) != -1){
-                forbidden = true;
+    chrome.storage.sync.get(["extension_data"], function(items){
+    var data = items.extension_data;
+        //alert(JSON.stringify(items));
+        if(data.isBlocked == true){
+            chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+            var url = tabs[0].url;
+            var forbidden = false;
+            for(var i=0;i<blacklist.length;i++){
+                if(url.search(blacklist[i]) != -1){
+                    forbidden = true;
+                }
             }
-        }
-        for(var i=0;i<whitelist.length;i++){
-            if(url.search(whitelist[i]) != -1){
-                forbidden = false;
+            for(var i=0;i<whitelist.length;i++){
+                if(url.search(whitelist[i]) != -1){
+                    forbidden = false;
+                }
             }
-        }
-        if(forbidden){
-            chrome.tabs.update(tabs[0].id, {url: "quit/quit.html"});
-        }
-    });  
-},1000);
+            if(forbidden){
+                chrome.tabs.update(tabs[0].id, {url: "quit/quit.html"});
+            }
+        });  
+    }
+});
+},3000);
+
+function updateStorage(obj){
+    chrome.storage.sync.get(["extension_data"], function(items){
+        var data = items.extension_data;
+        Object.keys(obj).forEach(function (key) {
+           data[key] = obj[key]; 
+        });
+        chrome.storage.sync.set({'extension_data': data});
+    });
+    
+}
+
 
