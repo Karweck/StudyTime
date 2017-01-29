0bbfeec906ed92ccb9c042c83c19459b335209cb
Adrian Steffan <Adrian Steffan>
2017-01-29 12:15:21 +0100

    sadasd

--- options/options.js
+++ options/options.js
@@ -2,8 +2,15 @@ $(".menu > div").click(function(){
     var id = $(this).attr("id");
     $(".tab").hide();
     $("#"+id+"-tab").show();
+	$(".menu > div").removeClass("active");
+	$(this).addClass("active");
 });
+
 if(location.hash != ""){
     $(".tab").hide();
     $("#"+location.hash.replace("#","")+"-tab").show();
+	$("#"+location.hash.replace("#","")).addClass("active");
+} else{
+	$("#settings-tab").show();
+	$("#settings").addClass("active");
 }
