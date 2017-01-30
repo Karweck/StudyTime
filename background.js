30884a351112ace413db2c3aa103fa8fad8f9914
Johannes Kunz <johannes.f.kunz@gmail.com>
2017-01-30 22:09:44 +0100

    Neue Datenstruktur

--- background.js
+++ background.js
@@ -1,31 +1,70 @@
+
+//MainLoop
+var initData = {
+	status: 0,
+	time: 0,
+    timer: {h:"00",m:"00",s:"00"},
+    workTimeline: [1,1,1,1],
+    isWorkTime: false,
+    isBlocked: false,
+    blacklist: ["minecraft","Spielaffe","1001Spiele","web.whatsapp","Facebook","Twitter","Amazon","Instagram","Netflix","Sport1","Apple","9gag","Wdr","Gamestar","Skype","Samsung","Prosieben","Pearl","Jetztspielen","Sat1","Nike","Pc-magazin","Parship","Youtube","Conrad"],
+    whitelist: []
+};
+
+
 setInterval(function(){
     chrome.storage.sync.get(["extension_data"], function(items){
-    var data = items.extension_data;
-        //alert(JSON.stringify(items));
-        if(data.isBlocked == true){
-            chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-            var url = tabs[0].url;
-            var forbidden = false;
-            var blacklist = data.blacklist;
-            var whitelist = data.whitelist;
-            for(var i=0;i<blacklist.length;i++){
-                if(url.search(blacklist[i].toLowerCase()) != -1){
-                    forbidden = true;
-                }
-            }
-            for(var i=0;i<whitelist.length;i++){
-                if(url.search(whitelist[i].toLowerCase()) != -1){
-                    forbidden = false;
-                }
-            }
-            if(forbidden){
-                chrome.tabs.update(tabs[0].id, {url: "quit/quit.html"});
-            }
-        });  
-    }
-});
+        if(JSON.stringify(items) == JSON.stringify({})){
+            chrome.storage.sync.set({'extension_data': initData});
+        }
+        
+        var data = items.extension_data;
+        var changes = {};
+        changes.timer = setTimer(data);
+        //index in der WorkTimeline
+        var zyklus = getZyklus(data);
+        if(zyklus%2==1){
+            changes.isWorkTime = false;
+            changes.isBlocked = false;
+        } else{
+            changes.isWorkTime = true;
+            changes.isBlocked = true;
+        }
+        if(zyklus%2==1 && data.isWorkTime==true){
+            alert("Pause!");
+        }
+        if(zyklus%2==0 && data.isWorkTime==false){
+            alert("Auf an die Arbeit!");
+        }
+        if(data.isBlocked == true && data.status == 1){
+            redirect(data);
+        }
+        updateStorage(changes);
+    });
 },500);
-
+//Functions
+function redirect(data){
+    
+     chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+        var url = tabs[0].url;
+        var forbidden = false;
+        var blacklist = data.blacklist;
+        var whitelist = data.whitelist;
+        for(var i=0;i<blacklist.length;i++){
+            if(url.search(blacklist[i].toLowerCase()) != -1){
+                forbidden = true;
+            }
+        }
+        for(var i=0;i<whitelist.length;i++){
+            if(url.search(whitelist[i].toLowerCase()) != -1){
+                forbidden = false;
+            }
+        }
+        if(forbidden){
+            chrome.tabs.update(tabs[0].id, {url: "quit/quit.html"});
+        }
+    });  
+}
 function updateStorage(obj){
     chrome.storage.sync.get(["extension_data"], function(items){
         var data = items.extension_data;
@@ -33,7 +72,47 @@ function updateStorage(obj){
            data[key] = obj[key]; 
         });
         chrome.storage.sync.set({'extension_data': data});
-    });
-    
+    }); 
+}
+function getZyklus(data){
+    var now = new Date().getTime();
+    var sec = Math.floor((now-data.time)/1000);
+    var min = Math.floor(sec/60);
+    var mins = 0;
+    for(var i=0;i<data.workTimeline.length*20;i++){
+        if(min<mins){
+            var stat = i%data.workTimeline.length;
+            break;
+        } else{
+            mins += data.workTimeline[i%data.workTimeline.length];
+        }
+    }
+    return stat-1;
+}
+
+function setTimer(data){
+    var now = new Date().getTime();
+    var sec = Math.floor((now-data.time)/1000);
+    var min = Math.floor(sec/60);
+    var mins = 0;
+    for(var i=0;i<data.workTimeline.length*20;i++){
+        if(min<mins){
+            var stat = i%data.workTimeline.length;
+            break;
+        } else{
+            mins += data.workTimeline[i%data.workTimeline.length];
+        }
+    }
+    var hours = "00";
+    var minutes = zeropad(Math.floor((mins-min-1)%60),2);
+    var seconds = zeropad(Math.floor((59-sec%60)),2);
+    return {h:hours,m:minutes,s:seconds};
+}
+function zeropad(integer,len){
+    string = integer + "";
+    while(string.length < len){
+        string = "0"+string;
+    }
+    return string;
 }
 
--- gui.js
+++ gui.js
@@ -1,105 +1,40 @@
-
-var urlCheckFrequency = 1000; //ms
 var colors = ["#92b745","#D9000F"];
 var statuses = ["Start Studying","Stop Studying?"];
-var timer;
-var time = 0;
+var headers = ["Arbeitszeit!","Pause!"];
+//Make data available to all EventListeners
+var data;
 
-var initData = {
-	status: 0,
-	time: 0,
-    workTimeline: [25,5,25,15],
-    isBlocked: false,
-    blacklist: ["minecraft","Spielaffe","1001Spiele","web.whatsapp","Facebook","Twitter","Amazon","Instagram","Netflix","Sport1","Apple","9gag","Wdr","Gamestar","Skype","Samsung","Prosieben","Pearl","Jetztspielen","Sat1","Nike","Pc-magazin","Parship","Youtube","Conrad"],
-    whitelist: []
-};
-	
-	
-chrome.storage.sync.get(["extension_data"], function(items){
-
-	if(JSON.stringify(items) == JSON.stringify({})){
-		chrome.storage.sync.set({'extension_data': initData});
-	}
-
-	var data = items.extension_data;
-	
-	$("#start-stop").html(statuses[data.status]);
-	$("#start-stop").css("background-color", colors[data.status]);
-
-	
-	$("#start-stop").click(function(){
-		
-			
-			
-		if(data.status == 0){
-            data.status = 1;
-			//show timer
-			if(data.time == 0){
-				time = new Date().getTime();
-			} else{
-				time = data.time;
-			}
-			$("#start-stop").click(function(){
-				var win = window.open("quit/quit.html", '_blank');
-				win.focus();
-			});
-			timer = setInterval(function(){
-				var now = new Date().getTime();
-			    setTimer(now-time,data.workTimeline);
-			},urlCheckFrequency);
-		} else{
-            var win = window.open("quit/quit.html", '_blank');
-			win.focus();
-        }
-			
-			
-		$("#start-stop").css("background-color", colors[data.status]);
-		$("#start-stop").html(statuses[data.status]);
-			
-		updateStorage({time:time,status:data.status});
-
-	});
-    
-	if(data.time != 0 && data.status == 1){
-		time = data.time;
-		timer = setInterval(function(){
-			var now = new Date().getTime();
-			setTimer(now-time,data.workTimeline);
-		},500);
-	} else{
-		timer = 0;
-	}
-	
-	function setTimer(time,timeline){
-		var sec = Math.round(time/1000);
-		var min = Math.floor(sec/60);
-		var hour = Math.floor(min/60);
-        var mins = 0;
-        for(var i=0;i<timeline.length*20;i++){
-            if(min<mins){
-                var stat = i%timeline.length;
-                break;
-            } else{
-                mins += timeline[i%timeline.length];
-            }
-        }
-        if(stat%2==1){
-            $("#modus").html("Study Time!");
-            $("#timer").css("color","red");
-            updateStorage({isBlocked:true});
+setInterval(function(){
+    chrome.storage.sync.get(["extension_data"], function(items){
+        data = items.extension_data;
+        $("#start-stop").html(statuses[data.status]);
+        $("#start-stop").css("background-color", colors[data.status]);
+        $("#start-stop").attr("name",data.status);
+        $("#modus").html(headers[data.isWorkTime ? 1 : 0]);
+        if(data.status == 1){
+            timer = data.timer;
+            $("#timer").html(timer.h+"h "+timer.m+"m "+timer.s+"s");
         } else{
-            $("#modus").html("Break!");
-            updateStorage({isBlocked:false});
-            $("#timer").css("color","green");
+            $("#timer").html("TIMER");
         }
-		$("#timer").html(Math.floor((mins-min)/60)+"h "+(mins-min-1)%60+"min "+(59-sec%60)+"s");
-	}
-});
+    });  
+},500);
+
 
 $("#close").click(function(){
 	window.close();
 });
-
+$("#start-stop").click(function(){
+    var changes = {};
+    if($(this).attr("name")=="0"){
+        changes.status = 1;
+        changes.time = new Date().getTime();
+    } else{
+        var win = window.open("quit/quit.html", '_blank');
+        win.focus();
+    }
+    updateStorage(changes);
+});
 
 function updateStorage(obj){
     chrome.storage.sync.get(["extension_data"], function(items){
@@ -109,5 +44,4 @@ function updateStorage(obj){
         });
         chrome.storage.sync.set({'extension_data': data});
     });
-    
 }
